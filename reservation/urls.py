f753332610beafb75746fd0da3dfa24a55badccd
primary pigments <primarypigments@gmail.com>
2024-02-28 13:29:45 +0000

    Add paths for ice reservation features

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,6 +1,10 @@
+from . import views
 from django.urls import path
-from .views import ReservationCreateView
+from .views import IceReservationCreateView, ice_reservation_home, available_ice_slots, submit_ice_reservation
+
 
 urlpatterns = [
-    path('new/', ReservationCreateView.as_view(), name='reservation_create'),
+    path('', ice_reservation_home, name='ice_reservation_home'),
+    path('availability/', available_ice_slots, name='availability'),
+    path('submit_ice_reservation/', submit_ice_reservation, name='submit_ice_reservation'),
 ]
