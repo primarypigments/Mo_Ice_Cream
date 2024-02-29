fc260ea8a0b7cbd98abf47c833dd9e6b85bb58a3
primary pigments <primarypigments@gmail.com>
2024-02-29 09:54:46 +0000

    Add class for data validation.

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,2 +1,9 @@
+from django import forms
+from .models import Reservation
+
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['ice_customer_name', 'ice_customer_email', 'date', 'ice_time_slot', 'is_cancelable']
         
         
