8d8ace024c58f266492bb21e336f048bdf80f802
primary pigments <primarypigments@gmail.com>
2024-03-04 11:22:45 +0000

    Enhance reservation admin display

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Reservation
 
-admin.site.register(Reservation)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("customer", "date", "time_slot", "location", "status")
