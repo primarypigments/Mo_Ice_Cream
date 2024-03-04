a47dc331276556be274a210582a4d08cf0974e49
primary pigments <primarypigments@gmail.com>
2024-03-04 11:26:01 +0000

    Add edit, delete, profile routes to urlpatterns

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,5 +3,20 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.reservation_page, name='ice_reservation_page'),
+    path("", views.reservation_page, name="reservation_page"),
+    path(
+        "edit_reservation/<int:id>/",
+        views.edit_reservation,
+        name="edit_reservation"
+    ),
+    path(
+        "delete_reservation/<int:id>/",
+        views.delete_reservation,
+        name="delete_reservation"
+    ),
+    path(
+        "profile/",
+        views.profile,
+        name="profile"
+    ),
 ]
