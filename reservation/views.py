79ce68ee769303986cd07d8b83852ef2e939f616
primary pigments <primarypigments@gmail.com>
2024-03-24 12:21:49 +0000

    Import IntegrityError from django.db

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
+from django.db import IntegrityError
 from .forms import ReservationForm 
 from .models import Reservation
 import logging
