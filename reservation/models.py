596c6901255d1e47f35c2373c5e0b54840a15c4c
primary pigments <primarypigments@gmail.com>
2024-03-24 12:05:21 +0000

    Add validationb to ensure date is non-null, non-blank, valid

--- reservation/models.py
+++ reservation/models.py
@@ -13,6 +13,7 @@ class Reservation(models.Model):
     max_length=17, validators=[
         validate_phone_number], null=False, blank=False)
     date = models.DateField(
+        null=False, blank=False, validators=[validate_future_date])
     time_slot = models.CharField(max_length=10, choices=[
         ('Morning', 'Morning'),
         ('Afternoon', 'Afternoon'),
