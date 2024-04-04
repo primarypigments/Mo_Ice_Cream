67a40928496dd2dc29cfbff5bd862ecbc7ec51a0
primary pigments <primarypigments@gmail.com>
2024-04-04 09:17:23 +0000

    Bug fix messages not posting to database

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Reservation
+from .models import ContactMessage
 
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ("customer", "date", "time_slot", "location", "status")
+
+
+admin.site.register(ContactMessage)
